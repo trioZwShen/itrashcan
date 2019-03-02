3cb0c7e2178ded904a91d63607d9086cd2adcb05
neilzwshen <neilzwshen@qq.com>
2019-03-02 12:56:53 +0800

    modify

--- config.sh
+++ config.sh
@@ -1,9 +1,4 @@
-# set the necessary path
-#  trashcan path
-export itrashcan_home="/home/szw/.trashcan"
-
-#  itrashcan path
-
 # set alias
-
-# set the path
+# rm="$itrashcan/delete "
+# rc=""
+# re=""
